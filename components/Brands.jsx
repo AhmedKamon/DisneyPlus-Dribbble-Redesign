3b8cc138ff29e0cf08f4ddac6340639962b55797
AhmedKamon <ahmedkamon3@gmail.com>
2022-06-30 11:20:01 +0600

    brands section complite

--- components/Brands.jsx
+++ components/Brands.jsx
@@ -6,10 +6,35 @@ function Brands() {
         <section className=' flex flex-col md:flex-row justify-center items-center mt-10 gap-6 px-8 max-w-[1400px] mx-auto'>
             <div className='brand group ' >
                 <Image src='/images/disnep.png' alt='brand image' layout='fill' objectFit='cover'  />
-                <video autoPlay loop playsInline className='hidden group-hover:inline rounded-lg object-cover md:w-screen' >
+                <video autoPlay loop playsInline className='hidden group-hover:inline rounded-lg object-cover w-screen' >
                     <source src="/videos/disney.mp4" type='video/mp4' />
                 </video>
             </div>
+            <div className='brand group ' >
+                <Image src='/images/pixar.png' alt='brand image' layout='fill' objectFit='cover'  />
+                <video autoPlay loop playsInline className='hidden group-hover:inline rounded-lg object-cover md:w-screen' >
+                    <source src="/videos/pixar.mp4" type='video/mp4' />
+                </video>
+            </div>
+            <div className='brand group ' >
+                <Image src='/images/marvel.png' alt='brand image' layout='fill' objectFit='cover'  />
+                <video autoPlay loop playsInline className='hidden group-hover:inline rounded-lg object-cover md:w-screen' >
+                    <source src="/videos/marvel.mp4" type='video/mp4' />
+                </video>
+            </div>
+            <div className='brand group ' >
+                <Image src='/images/starwars.png' alt='brand image' layout='fill' objectFit='cover'  />
+                <video autoPlay loop playsInline className='hidden group-hover:inline rounded-lg object-cover md:w-screen' >
+                    <source src="/videos/star-wars.mp4" type='video/mp4' />
+                </video>
+            </div>
+            <div className='brand group ' >
+                <Image src='/images/national-geographic.png' alt='brand image' layout='fill' objectFit='cover'  />
+                <video autoPlay loop playsInline className='hidden group-hover:inline rounded-lg object-cover md:w-screen' >
+                    <source src="/videos/national-geographic.mp4" type='video/mp4' />
+                </video>
+            </div>
+            
         </section>
     )
 }
