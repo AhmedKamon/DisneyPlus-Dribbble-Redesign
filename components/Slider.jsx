535dd9147ff526cb0d3d6a01c07ab3ad3657d492
AhmedKamon <ahmedkamon3@gmail.com>
2022-06-30 10:23:44 +0600

    slider done

--- components/Slider.jsx
+++ components/Slider.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import { Carousel } from 'react-responsive-carousel'
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 function Slider() {
   return (
-    <section>
-        
+    <section className=' relative mt-7 shadow-2xl max-w-screen-2xl mx-auto' >
+          <Carousel
+          autoPlay
+          infiniteLoop
+          showStatus={false}
+          showIndicators={false}
+          showThumbs={false}
+          interval={5000}
+          
+          >
+                <div>
+                    <img loading='lazy' src='/images/slider-3.jpg' alt='movie slider' />
+                </div>
+                <div>
+                    <img loading='lazy' src='/images/slider-2.jpg' alt='movie slider' />
+                </div>
+                <div>
+                    <img loading='lazy' src='/images/slider-1.jpg' alt='movie slider' />
+                </div>
+                <div>
+                    <img loading='lazy' src='/images/slider-4.jpeg' alt='movie slider' />
+                </div>
+                
+            </Carousel>
     </section>
   )
 }
