0c7a233942a3b857c23d8c4727b77bb1ef220598
AhmedKamon <ahmedkamon3@gmail.com>
2022-06-30 11:02:57 +0600

    brands problem solved

--- components/Brands.jsx
+++ components/Brands.jsx
@@ -1,9 +1,15 @@
+import Image from 'next/image'
 import React from 'react'
 
 function Brands() {
     return (
-        <section className=' flex flex-col md:flex-row '>
-            
+        <section className=' flex flex-col md:flex-row justify-center items-start mt-10 gap-6 px-8 max-w-[1400px] mx-auto'>
+            <div className='brand group ' >
+                <Image src='/images/disnep.png' alt='brand image' layout='fill' objectFit='cover'  />
+                <video autoPlay loop playsInline className='hidden group-hover:inline rounded-lg object-cover' >
+                    <source src="/videos/disney.mp4" />
+                </video>
+            </div>
         </section>
     )
 }
