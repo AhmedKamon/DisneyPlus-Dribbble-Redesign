61a1ffe41c2898cc8bb8a69cf89f9737a1ab6b03
AhmedKamon <ahmedkamon3@gmail.com>
2022-06-30 11:13:09 +0600

    first brand css

--- components/Brands.jsx
+++ components/Brands.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 
 function Brands() {
     return (
-        <section className=' flex flex-col md:flex-row justify-center items-start mt-10 gap-6 px-8 max-w-[1400px] mx-auto'>
+        <section className=' flex flex-col md:flex-row justify-center items-center mt-10 gap-6 px-8 max-w-[1400px] mx-auto'>
             <div className='brand group ' >
                 <Image src='/images/disnep.png' alt='brand image' layout='fill' objectFit='cover'  />
-                <video autoPlay loop playsInline className='hidden group-hover:inline rounded-lg object-cover' >
-                    <source src="/videos/disney.mp4" />
+                <video autoPlay loop playsInline className='hidden group-hover:inline rounded-lg object-cover md:w-screen' >
+                    <source src="/videos/disney.mp4" type='video/mp4' />
                 </video>
             </div>
         </section>
