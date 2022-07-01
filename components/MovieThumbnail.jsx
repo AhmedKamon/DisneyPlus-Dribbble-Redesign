b67bec09b6896e16467b8707ee769fcb8ac4b873
AhmedKamon <ahmedkamon3@gmail.com>
2022-07-01 10:51:54 +0600

    tailwind scrollbar hidden packge

--- components/MovieThumbnail.jsx
+++ components/MovieThumbnail.jsx
@@ -1,8 +1,16 @@
+import Image from 'next/image'
+import { useRouter } from 'next/router'
 import React from 'react'
 
 function MovieThumbnail({result}) {
+    const BASE_URL = "https://image.tmdb.org/t/p/original/"
+    const router = useRouter()
   return (
-    <div>MovieThumbnail</div>
+    <div className=' flex min-w-[250px] min-h-[170px] md:min-w-[330px] md:min-h-[210px] rounded-lg overflow-hidden  border-[3px] border-[#f9f9f9] border-opacity-10 hover:shadow-2xl hover:border-opacity-80 transform hover:scale-105 transition duration-300 cursor-pointer '>
+        <Image src={
+        `${BASE_URL}${result.backdrop_path || result.poster_path}` ||  `${BASE_URL}${result.poster_path}`
+    } width={330} height={210} alt={result.title}/>
+    </div>
   )
 }
 
--- components/MoviesCollection.jsx
+++ components/MoviesCollection.jsx
@@ -5,11 +5,14 @@ function MoviesCollection({results, title}) {
   return (
     <div className='relative flex flex-col space-y-2 my-10 px-8 max-w-[1400px] mx-auto '>
       <h2 className='font-semibold'>{title}</h2>
+      <div className='flex space-x-6 scrollbar-hide overflow-x-scroll  overflow-y-hidden'>
+
       {
         results.map((result) => (
           <MovieThumbnail result={result} key={result.id} />
-        ))
-      }
+          ))
+        }
+        </div>
     </div>
   )
 }
