a16470d662cedda4ae42ccc78cc7a0cd1553b6d9
AhmedKamon <ahmedkamon3@gmail.com>
2022-07-01 10:58:41 +0600

    scroll-x not working

--- components/MoviesCollection.jsx
+++ components/MoviesCollection.jsx
@@ -5,7 +5,7 @@ function MoviesCollection({results, title}) {
   return (
     <div className='relative flex flex-col space-y-2 my-10 px-8 max-w-[1400px] mx-auto '>
       <h2 className='font-semibold'>{title}</h2>
-      <div className='flex space-x-6 scrollbar-hide overflow-x-scroll  overflow-y-hidden'>
+      <div className='flex space-x-6 scrollbar-hide overflow-x-scroll  overflow-y-hidden p-2 -m-2'>
 
       {
         results.map((result) => (
