b62c962f53bd0af86267024263f781b314cafaf0
AhmedKamon <ahmedkamon3@gmail.com>
2022-07-02 09:29:48 +0600

    movie and show collection

--- components/ShowsCollection.jsx
+++ components/ShowsCollection.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import MovieThumbnail from './MovieThumbnail'
 
-function ShowsCollection() {
+function ShowsCollection({results, title}) {
     return (
-        <div>
-            ShowsCollection
+        <div className='relative flex flex-col space-y-2 my-10 px-8 max-w-[1400px] mx-auto '>
+          <h2 className='font-semibold'>{title}</h2>
+          <div className='flex space-x-6 scrollbar-hide overflow-x-scroll  overflow-y-hidden p-2 -m-2'>
+    
+          {
+            results.map((result) => (
+              <MovieThumbnail result={result} key={result.id} />
+              ))
+            }
+            </div>
         </div>
-    )
+      )
 }
 
 export default ShowsCollection
